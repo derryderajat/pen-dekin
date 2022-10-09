5d5aa109bc2f382800d55ddc566c0c614a360889
derry derajat <derryderajat@mail.com>
2022-10-09 18:43:46 +0700

    trakteer fix to button

--- Components/Trakteer.js
+++ Components/Trakteer.js
@@ -10,17 +10,14 @@ function Trakteer() {
       <h1 className='text-3xl font-bold text-white mb-4'>
         Help me buy a Coffee:
       </h1>
-      <div className='bg-[#be1e2d] rounded-md hover:cursor-pointer w-fit h-fit px-4 py-2 flex flex-row items-center'>
-        <div
-          className='relative h-6 w-4 animate-pulse'
-          onClick={(e) => router.push('https://trakteer.id/derryderajat/tip')}
-        >
-          <Image src={TrakteerIcon} alt='trakteer' layout='fill' />
-        </div>
-        <h2 className='ml-2 font-semibold text-white'>
+      <button
+        className='bg-[#be1e2d]  rounded-md hover:cursor-pointer w-fit h-fit px-4 py-2 flex flex-row items-center'
+        onClick={(e) => router.push('https://trakteer.id/derryderajat/tip')}
+      >
+        <h2 className='ml-2 font-semibold text-white animate-pulse'>
           Support me in Trakteer
         </h2>
-      </div>
+      </button>
     </div>
   );
 }
